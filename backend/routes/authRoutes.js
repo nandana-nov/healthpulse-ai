c36fa057986f5d194b95b2b6cd2d8b6df7ce46ae
nandananov <nandananovjp03@gamil.com>
2026-05-18 10:55:24 +0530

    fixed user model import

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 const router = express.Router();
 
